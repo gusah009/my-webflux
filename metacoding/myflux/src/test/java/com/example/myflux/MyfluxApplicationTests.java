4a607444823e30af71c12ce309ab50232b737e5f
정핸모 <gusah009@naver.com>
2022-11-24 08:29:52 +0900

    메타코딩: project init

---
+++ metacoding/myflux/src/test/java/com/example/myflux/MyfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
