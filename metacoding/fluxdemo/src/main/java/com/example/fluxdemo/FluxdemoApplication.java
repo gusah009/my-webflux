0b96921c9528290c2e1e7820f0ac6661d09180bb
정핸모 <gusah009@naver.com>
2022-11-27 15:13:40 +0900

    메타코딩: webflux project init

---
+++ metacoding/fluxdemo/src/main/java/com/example/fluxdemo/FluxdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fluxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxdemoApplication.class, args);
+    }
+
+}
---
+++ metacoding/fluxdemo/src/test/java/com/example/fluxdemo/FluxdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fluxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
